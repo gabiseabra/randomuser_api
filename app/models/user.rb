c2d3a4e6c00deb61c4fe779637a2f6e54d158f74
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-17 20:57:42 -0200

    Capitalize names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,18 @@ class User < ApplicationRecord
 
   JSON = %i[id title name email phone cel avatar].freeze
 
+  before_save :capitalize_names
+
   mount_uploader :avatar, AvatarUploader
 
   scope :search, ->(term) { like_any(%i[name email], term) }
 
   self.per_page = 15
+
+  private
+
+  def capitalize_names
+    self.name = name.split.map(&:capitalize).join(' ')
+    self.title = title.capitlize
+  end
 end
